fb08f72c9789549ac0dca7e4ecfa1ea97eaeb4ce
Carlos Carvalho <cccarv82@gmail.com>
2025-03-24 19:00:18 -0300

    Atualizado para versão 1.6.5 com correções no modo de compatibilidade simples

--- pkg/util/version.go
+++ pkg/util/version.go
@@ -6,7 +6,7 @@ const (
 	// Name é o nome do aplicativo
 	Name = "CompressVideo"
 	// Version é a versão do aplicativo
-	Version = "1.6.4"
+	Version = "1.6.5"
 	// BuildDate é a data em que o aplicativo foi compilado
 	BuildDate = "development"
 )
